4bfaebc242cd27783ea09355617fdb5c720cc69c
trainer2edureka <trainer2edureka@gmail.com>
2020-10-13 06:19:40 +0000

    Changes done through branch1

--- Demo.java
+++ Demo.java
@@ -4,8 +4,10 @@ public static void main(String[] a)
 {
 System.out.println("First file created");
 System.out.println("Modified file created");
-System.out.println("First file created");
-System.out.println("First file created");
+
+
+System.out.println("Changes done in branch file");
+System.out.println("2 Changes done in branch file");
 
 
 
